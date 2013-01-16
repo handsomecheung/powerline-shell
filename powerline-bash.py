9a6cb9f9b8c58c4c78ee843ed0cf87c920fd58ee
handsomecheung <handsomecheung@gmail.com>
2013-01-16 15:48:09 +0800

    optimize code
    - if len(is_svn_output) != 0:
    + if is_svn_output:

--- powerline-bash.py
+++ powerline-bash.py
@@ -226,7 +226,7 @@ def add_git_segment(powerline, cwd):
 def add_svn_segment(powerline, cwd):
     is_svn = subprocess.Popen(['svn', 'status'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     is_svn_output = is_svn.communicate()[1].strip()
-    if len(is_svn_output) != 0:
+    if is_svn_output:
         return
 
     '''svn info:
